344744aaf6904d324df171011ea80f711d2573ec
kartikprakash00 <113478210+kartikprakash00@users.noreply.github.com>
2023-08-26 14:35:32 +0530

    Add files via upload

---
+++ counter-master/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { CounterPage } from './pages/Counterpage';
+
+function App() {
+  return (
+    <CounterPage/>
+  );
+}
+
+export default App;
