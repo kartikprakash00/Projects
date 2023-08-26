dbe07f4ddcab831e3f8920793c3f11f9db921c67
kartikprakash00 <kartikprakash1@gmail.com>
2023-08-26 14:53:20 +0530

    assignment

--- counter-master/src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-export const  Button = ({val, classname , fn})=>{
-    const clicked = ()=>{
-    fn(val);
-    console.log('Clicked Happen',val);
-    }
- return (<button onClick={clicked} className = {classname}>{val}</button>) 
-}
